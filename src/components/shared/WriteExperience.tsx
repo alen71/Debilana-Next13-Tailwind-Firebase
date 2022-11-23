52c2ac18ff3f5ae6ebefe4c4925ecaa1f9d1ec89
Tizu18 <alen_babanovic123@hotmail.com>
2022-11-23 19:55:07 +0100

    test

--- src/components/shared/WriteExperience.tsx
+++ src/components/shared/WriteExperience.tsx
@@ -8,7 +8,7 @@ const WriteExperience = () => {
   const { theme } = useContext(ThemeContext)
 
   return (
-    <div className="flex items-center gap-3 py-1 px-3 cursor-pointer rounded-md hover:bg-primary-light">
+    <div className="flex items-center gap-3 py-1 px-3 cursor-pointer rounded-md hover:bg-primary-light dark:hover:text-gray-dark">
       <span className="hidden md:inline-flex">Podeli iskustvo</span>
       <Pencil />
     </div>
