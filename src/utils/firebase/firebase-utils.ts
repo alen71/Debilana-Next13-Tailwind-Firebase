ceaf132d7f1e0829473c80159a5057c6cf7b7952
Tizu18 <alen_babanovic123@hotmail.com>
2022-12-07 17:19:37 +0100

    vercel env

--- src/utils/firebase/firebase-utils.ts
+++ src/utils/firebase/firebase-utils.ts
@@ -18,12 +18,12 @@ import {
 import { getDatabase, ref, set, update } from 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID
+  apiKey: process.env.NEXT_PUBLIC_VERCEL_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_VERCEL_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_VERCEL_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_VERCEL_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_VERCEL_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_VERCEL_APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
