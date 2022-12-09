ffdb8f1c8424b85d4063a316cedbf3df9c18a7b1
Tizu18 <alen_babanovic123@hotmail.com>
2022-12-09 15:35:39 +0100

    tesst

--- next.config.js
+++ next.config.js
@@ -4,6 +4,21 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          }
+        ]
+      }
+    ]
+  },
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
