2c13a57ea4f28638468cb5a3b4763b00e44b293b
Tizu18 <alen_babanovic123@hotmail.com>
2023-01-22 10:06:20 +0100

    write experience hover issue

--- src/components/shared/WriteExperience.tsx
+++ src/components/shared/WriteExperience.tsx
@@ -8,7 +8,7 @@ const WriteExperience = () => {
     <Link href="/create-post">
       <div className="group flex items-center text-sm xl:text-base gap-3 lg:py-[6px] lg:pl-4 lg:pr-2 cursor-pointer rounded-lg lg:bg-primary lg:hover:bg-primary-light">
         <span className="hidden lg:inline-flex text-white">Napravi objavu</span>
-        <div className="w-8 h-8 rounded-md lg:w-5 lg:h-5 flex justify-center items-center lg:group-hover:animate-wiggle bg-primary hover:bg-primary-light text-white lg:text-white lg:hover:text-white scale-100 sm:scale-110 lg:scale-100">
+        <div className="w-8 h-8 rounded-md lg:w-5 lg:h-5 flex justify-center items-center lg:group-hover:animate-wiggle bg-primary group-hover:bg-primary-light text-white lg:text-white lg:hover:text-white scale-100 sm:scale-110 lg:scale-100">
           <Pencil />
         </div>
       </div>
