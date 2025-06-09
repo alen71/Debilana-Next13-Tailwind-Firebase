f5ec6d12e5dade754ee06a652c68d33f2c5eb8e5
exodusdev <stefan@agriot.group>
2025-06-09 20:17:13 +0200

    Update dependencies, add search functionality, and refactor navbar components

--- src/components/shared/SearchBar.tsx
+++ src/components/shared/SearchBar.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 
 import LupaSvg from '../../assets/lupa.svg'
+import { cn } from '@/lib/utils/common'
+import { useSearchStore } from '@/store/useSearchStore'
+import { useSearchPost } from '@/hooks/useSearchPost'
 
 type Props = {
   hide?: boolean
@@ -8,35 +11,63 @@ type Props = {
 
 const SearchBar = ({ hide }: Props) => {
   const [isFocused, setIsFocused] = useState(false)
+  const [searchText, setSearchText] = useState('')
+
+  const clearSearch = useSearchStore(state => state.clearSearch)
 
   const setFocus = () => {
     setIsFocused(!isFocused)
   }
 
-  const inputColor = isFocused
-    ? 'text-black border-black dark:border-primary-dark dark:text-primary-dark'
-    : 'text-gray-text-hover border-gray-text-hover'
-  const placeholderColor = isFocused
-    ? 'placeholder:text-black placeholder:dark:text-primary-dark'
-    : 'placeholder:text-gray-text-hover'
+  const { getSearchResults } = useSearchPost()
 
-  const isHide = hide ? 'hidden' : ''
+  const onClearSearch = () => {
+    setSearchText('')
+    clearSearch()
+  }
+
+  const onSearch = () => {
+    getSearchResults(searchText)
+  }
 
   return (
-    <div
-      className={`${inputColor} ${isHide} rounded-lg w-full sm:max-w-[400px] h-8 xl:h-[38px] overflow-hidden border-[1px] flex items-center gap-[14px] pl-2 xl:pl-[14px] `}
-    >
-      <button className="cursor-pointer">
-        <LupaSvg className="scale-75 xl:scale-100" />
-      </button>
+    <div className="flex flex-col lg:flex-row items-center gap-2">
+      <div
+        className={cn(
+          'rounded-lg w-full sm:max-w-[400px] min-w-[300px] sm:min-w-[350px]  xl:min-w-[500px] h-8 xl:h-[38px] overflow-hidden border-[1px] flex items-center gap-[14px] pl-2 xl:pl-[14px]',
+          isFocused && 'border-white',
+          hide && 'hidden'
+        )}
+      >
+        <button className="cursor-pointer">
+          <LupaSvg className="scale-75 xl:scale-100" />
+        </button>
 
-      <input
-        type="text"
-        placeholder="Pretraži...."
-        className={`${placeholderColor} placeholder:text-sm xl:placeholder:text-base w-full bg-transparent outline-none`}
-        onFocus={setFocus}
-        onBlur={setFocus}
-      />
+        <input
+          type="text"
+          placeholder="Pretraži...."
+          className={cn(
+            `placeholder:text-gray-text-hover placeholder:text-sm xl:placeholder:text-base w-full bg-transparent outline-none`
+          )}
+          onFocus={setFocus}
+          onBlur={setFocus}
+          value={searchText}
+          onChange={e => setSearchText(e.target.value)}
+        />
+      </div>
+
+      <div className="flex flex-row items-center gap-4">
+        <button
+          className="cursor-pointer bg-primary text-white rounded-lg px-5 py-1"
+          onClick={onSearch}
+        >
+          Pretraži
+        </button>
+
+        <button className="cursor-pointer" onClick={onClearSearch}>
+          <span className="dark:text-white">Poništi</span>
+        </button>
+      </div>
     </div>
   )
 }
---
+++ src/utils/firebase/algolia.ts
@@ -0,0 +1,28 @@
+import { algoliasearch, SearchResponse } from 'algoliasearch'
+import { IPost } from '../types/posts.types'
+
+const appId = process.env.NEXT_PUBLIC_VERCEL_APP_ID ?? ''
+const apiKey = process.env.NEXT_PUBLIC_VERCEL_API_KEY ?? ''
+
+const client = algoliasearch(appId, apiKey)
+
+console.log(apiKey)
+
+client.setSettings({
+  indexName: 'debilana_content',
+  indexSettings: {
+    attributesForFaceting: ['searchable(content)']
+  }
+})
+
+export const searchRequest = (
+  query: string
+): Promise<SearchResponse<IPost>> => {
+  return client.searchSingleIndex({
+    indexName: 'debilana_content',
+    searchParams: {
+      facetFilters: ['content'],
+      query
+    }
+  })
+}
